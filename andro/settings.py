027a10c9b321398a9b6989e9b381e65d0f784345
Serginio3 <hoyan.kek@gmail.com>
2020-05-06 00:55:12 +0300

    jpeg

--- andro/settings.py
+++ andro/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = (
     'blog',
 )
 
-print(os.walk(BASE_DIR))
-
 MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -104,12 +102,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
