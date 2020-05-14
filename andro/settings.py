f8787850895d0c66c88abc863b878e8a62777b3d
Serginio3 <hoyan.kek@gmail.com>
2020-05-14 12:41:40 +0300

    shop

--- andro/settings.py
+++ andro/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-#
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
 
@@ -150,4 +150,4 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'andro.storage_backends.MediaStorage'
