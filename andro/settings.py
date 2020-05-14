ae084d5e035dc32cee80863f9f8d7861c8fdd5ad
Serginio3 <hoyan.kek@gmail.com>
2020-05-14 13:49:06 +0300

    add setting.py

--- andro/settings.py
+++ andro/settings.py
@@ -111,8 +111,7 @@ USE_TZ = True
 # )
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 CART_SESSION_ID = 'cart'
 
@@ -131,8 +130,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_URL = '/static/'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'andro/static'),
 ]
@@ -149,6 +148,9 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+MEDIA_URL = "https://%s.s3.amazonaws.com/" % os.environ['AWS_STORAGE_BUCKET_NAME']
+MEDIA_ROOT = ''
 
 DEFAULT_FILE_STORAGE = 'andro.storage_backends.MediaStorage'
 AWS_DEFAULT_ACL = None
