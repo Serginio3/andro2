d8a192e3d88522dcb4a97fd801364e71a6ad8ca3
Serginio3 <hoyan.kek@gmail.com>
2020-05-14 13:09:44 +0300

    shop

--- andro/urls.py
+++ andro/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     url(r'^orders/', include('orders.urls', namespace='orders')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^', include('shop.urls', namespace='shop'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
