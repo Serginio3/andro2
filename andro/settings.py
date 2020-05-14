9b23fc27cc2e875302274ff892b068437633f862
Serginio3 <hoyan.kek@gmail.com>
2020-05-14 13:52:10 +0300

    add setting.py

--- andro/settings.py
+++ andro/settings.py
@@ -150,5 +150,8 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
+MEDIA_URL = "https://%s.s3.amazonaws.com/" % os.environ['AWS_STORAGE_BUCKET_NAME']
+MEDIA_ROOT = ''
+
 DEFAULT_FILE_STORAGE = 'andro.storage_backends.MediaStorage'
 AWS_DEFAULT_ACL = None
